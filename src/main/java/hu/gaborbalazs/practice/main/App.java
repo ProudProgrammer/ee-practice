19deefd5363ebd5dbeb03b145aaa8526078b92de
Balázs Gábor <mail.gabor.balazs@gmail.com>
2017-05-21 14:03:26 +0200

    Project creation

---
+++ src/main/java/hu/gaborbalazs/practice/main/App.java
@@ -0,0 +1,19 @@
+package hu.gaborbalazs.practice.main;
+
+import java.sql.SQLException;
+
+public class App {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		System.out.println("Hello World");
+		
+//		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gb_practice?useSSL=false", "root", "root");
+//		Connection conn = DriverManager.getConnection("jdbc:h2:mem:test");
+//		Statement stmt = conn.createStatement();
+		
+		System.out.println("OK");
+
+	}
+
+}
